a53c4d02c0698c8c3eaf07e1772ef8b0e891a856
Row-Bear <60758353+Row-Bear@users.noreply.github.com>
2023-12-22 16:48:13 +0100

    Update astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'hybrid'
+    site: 'https://row-bear.github.io',
+    base: '/frontend',
 });
